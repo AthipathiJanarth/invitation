bd69b7c62e82626f42258c90054444547c8dfd5f
Athipu <36738502+Athipu@users.noreply.github.com>
2021-07-14 14:04:28 +0530

    Timer removal

--- invite/js/main.js
+++ invite/js/main.js
@@ -277,27 +277,6 @@
 
 		var timeLeft = endTime - now;
 		
-		if(timeLeft>0){
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
-
-		$("#days").html(days + "<span>Days</span>");
-		$("#hours").html(hours + "<span>Hours</span>");
-		$("#minutes").html(minutes + "<span>Minutes</span>");
-		$("#seconds").html(seconds + "<span>Seconds</span>");	
-		}
-		else{
-			$("#timer").html("<h2 class='txt-mrg'>Thanks For Celebrating with us!!</h2>");
-			$("#change-txt").html("<span class='subheading'>We Are Married</span>")
-		}
-			
-
 }
 
 setInterval(function() { makeTimer(); }, 1000);
